12ef563229381e180bbad06a7d8b773f195d8495
Jonathan Lehr <jonathan@aboutobjects.com>
2024-06-12 18:32:17 -0400

    @Observable support; made Book a class

--- Catalogs/BookModel/Book.swift
+++ Catalogs/BookModel/Book.swift
@@ -2,8 +2,21 @@
 // See LICENSE.txt for this project's licensing information.
 
 import Foundation
+import Observation
 
-struct Book: Codable, Identifiable, Hashable {
+@Observable class Book: Codable, Identifiable/*, Hashable*/ {
+    //    static func == (lhs: Book, rhs: Book) -> Bool {
+    //        lhs.id == rhs.id
+    //    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case year
+        case author
+        case percentComplete
+    }
+    
     var id = UUID()
     var title: String
     var year: String
@@ -15,16 +28,40 @@ struct Book: Codable, Identifiable, Hashable {
         let url = Bundle.main.path(forResource: title, ofType: "jpg") ?? ""
         return URL(fileURLWithPath: url)
     }
-//    
-//    init(id: UUID = UUID(),
-//         title: String = "",
-//         year: String = "",
-//         author: String = "",
-//         percentComplete: Double = 0.0)
-//    {
-//        self.title = title
-//        self.year = year
-//        self.author = author
-//        self.percentComplete = percentComplete
+    
+    init(id: UUID = UUID(),
+         title: String = "",
+         year: String = "",
+         author: String = "",
+         percentComplete: Double = 0.0)
+    {
+        self.title = title
+        self.year = year
+        self.author = author
+        self.percentComplete = percentComplete
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        year = try container.decode(String.self, forKey: .year)
+        author = try container.decode(String.self, forKey: .author)
+        percentComplete = try container.decodeIfPresent(Double.self, forKey: .percentComplete) ?? 0.0
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(title, forKey: .title)
+        try container.encode(year, forKey: .year)
+        try container.encode(author, forKey: .author)
+        try container.encode(percentComplete, forKey: .percentComplete)
+    }
+
+
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(id)
 //    }
+    
 }
--- Catalogs/BookModel/BookCatalog.swift
+++ Catalogs/BookModel/BookCatalog.swift
@@ -2,17 +2,54 @@
 // See LICENSE.txt for this project's licensing information.
 
 import Foundation
+import Observation
 
-class BookCatalog: Codable, Identifiable, CustomStringConvertible
+@Observable class BookCatalog: Codable, Identifiable, CustomStringConvertible
 {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case books
+    }
+    
     var id = UUID()
     var title: String
     var books: [Book]
-        
-    var description: String { "\n\(BookCatalog.self):\n\ttitle: \(title)\n\tbooks: \(books)\n" }
+    
+    var hasBooks: Bool { !books.isEmpty }
+    var description: String {
+        "\n\(BookCatalog.self):\n\ttitle: \(title)\n\tbooks: \(books)\n"
+    }
     
     init(title: String, books: [Book]) {
         self.title = title
         self.books = books
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        books = try container.decode([Book].self, forKey: .books)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(title, forKey: .title)
+        try container.encode(books, forKey: .books)
+    }
+}
+
+// MARK: - Actions
+extension BookCatalog {
+    
+    func remove(atOffsets indexSet: IndexSet) {
+        books.remove(atOffsets: indexSet)
+        save()
+    }
+    
+    func save() {
+        // TODO: implement me!
     }
 }
