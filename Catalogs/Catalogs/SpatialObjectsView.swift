f74cae3364fb294d9824ce56dfbc562ee7ed5470
Jonathan Lehr <jonathan@aboutobjects.com>
2025-01-29 11:40:05 -0500

    Cleanups

--- Catalogs/Catalogs/SpatialObjectsView.swift
+++ Catalogs/Catalogs/SpatialObjectsView.swift
@@ -10,52 +10,57 @@ struct SpatialObjectsView: View {
     
     var viewModel: CatalogsViewModel
     
-    @State private var currentTransform: Transform?
-    @State private var currentMagnification: SIMD3<Float>?
+    @State private var baseTransform: Transform?
     
-    private let startPosition: SIMD3<Float> = SIMD3(x: 0, y: 1.5, z: -1.5)
+    private let defaultPosition: SIMD3<Float> = [0, 1.5, -1.5]
     
     var body: some View {
         
         RealityView { _ in
-            // We don't have any default content to load.
+            // No default content
         } update: { content in
-            let entity = loadSelectedObject()
-            content.add(entity)
+            if let entity = loadSelectedObject() {
+                content.add(entity)
+            } else {
+                print("Unable to load entity named \(viewModel.selectedObject?.title ?? "null")")
+            }
         } placeholder: {
             ProgressView()
         }
         .gesture(dragGesture)
         .simultaneousGesture(rotateGesture)
-        .simultaneousGesture(magnifyGesture)
         .simultaneousGesture(doubleTapGesture)
         .simultaneousGesture(tripleTapGesture)
     }
     
-    private func loadSelectedObject() -> Entity {
+    private func loadSelectedObject() -> Entity? {
         guard let url = viewModel.selectedObject?.modelUrl,
               let entity = try? Entity.load(contentsOf: url)
         else {
             print("Unable to load \(String(describing: viewModel.selectedObject?.modelUrl))")
-            return Entity()
+            return nil
         }
         
-        entity.position = startPosition
         configure(entity: entity)
         
-        print(entity)
+        // .print(entity)
         return entity
     }
     
     private func configure(entity: Entity) {
          
-        // Hover effect
-        // entity.components.set(HoverEffectComponent())
-        
+        entity.position = defaultPosition
+        entity.scale = entity.scale(relativeTo: nil) * 2 // [0.02, 0.02, 0.02]
+
         // Handling gestures
         entity.generateCollisionShapes(recursive: true)
-        entity.components.set(InputTargetComponent(allowedInputTypes: .indirect))
+        entity.components.set(InputTargetComponent())
         
+        // Custom components
+        if entity.hasAnimation {
+            entity.components.set(MyAnimationComponent())
+        }
+
         // Casting shadows
         entity.enumerateHierarchy { entity in
             if entity is ModelEntity {
@@ -64,108 +69,18 @@ struct SpatialObjectsView: View {
         }
         
         // Image-based lighting
-        Task {
-            guard let resource = try? await EnvironmentResource(named: "Sunlight") else { return }
-            let component = ImageBasedLightComponent(source: .single(resource),
-                                                     intensityExponent: 9)
-            
-            entity.components.set(component)
-            entity.components.set(ImageBasedLightReceiverComponent(imageBasedLight: entity))
-        }
-        
-        // Custom components
-        if entity.hasAnimation {
-            entity.components.set(MyAnimationComponent())
-        }
-
-        entity.scale = entity.scale(relativeTo: nil) * 2 // [0.02, 0.02, 0.02]
-        print("Scale is \(entity.scale)")
+        //        Task {
+        //            guard let resource = try? await EnvironmentResource(named: "Sunlight") else { return }
+        //            let component = ImageBasedLightComponent(source: .single(resource),
+        //                                                     intensityExponent: 9)
+        //
+        //            entity.components.set(component)
+        //            entity.components.set(ImageBasedLightReceiverComponent(imageBasedLight: entity))
+        //        }
     }
 }
 
-// MARK: - Moving an entity
-extension SpatialObjectsView {
-    
-    var dragGesture: some Gesture {
-        DragGesture()
-            .targetedToAnyEntity()
-            .onChanged { value in
-                let entity = value.entity
-                if currentTransform == nil {
-                    currentTransform = entity.transform
-                }
-                
-                let translation = value.convert(value.translation3D, from: .local, to: entity.parent!)
-                entity.transform.translation = currentTransform!.translation + translation
-            }
-            .onEnded { _ in
-                currentTransform = nil
-            }
-    }
-}
-
-// MARK: - Removing an entity
-extension SpatialObjectsView {
-        
-    var tripleTapGesture: some Gesture {
-        TapGesture(count: 3)
-            .targetedToAnyEntity()
-            .onEnded { value in
-                value.entity.removeFromParent()
-            }
-    }
-}
-
-// MARK: - Modifying Geometry
-extension SpatialObjectsView {
-    
-    var rotateGesture: some Gesture {
-        RotateGesture3D(constrainedToAxis: .y)
-            .targetedToAnyEntity()
-            .onChanged { value in
-                let entity = value.entity
-                if currentTransform == nil {
-                    currentTransform = entity.transform
-                }
-                
-                let transform = Transform(AffineTransform3D(rotation: value.rotation))
-                entity.transform.rotation = currentTransform!.rotation * transform.rotation
-            }
-            .onEnded { _ in
-                currentTransform = nil
-            }
-    }
-    
-    var magnifyGesture: some Gesture {
-        MagnifyGesture()
-            .targetedToAnyEntity()
-            .onChanged { value in
-                if currentMagnification == nil {
-                    currentMagnification = value.entity.scale
-                }
-                
-                let delta = Float(value.magnification - 1)
-                value.entity.scale = max(currentMagnification! + .init(repeating: delta), 0.2)
-            }
-            .onEnded { _ in
-                currentMagnification = nil
-            }
-    }
-}
-
-// MARK: - Animation
-extension SpatialObjectsView {
-    
-    var doubleTapGesture: some Gesture {
-        TapGesture(count: 2)
-            .targetedToAnyEntity()
-            .onEnded { value in
-                value.entity.toggleAnimation()
-            }
-    }
-}
-
-// MARK: - Custom Stuff
+// MARK: - Animation Support
 struct MyAnimationComponent: Component {
     
     var isAnimating = false
@@ -191,6 +106,61 @@ extension Entity {
         components[MyAnimationComponent.self] = MyAnimationComponent(isAnimating: !isAnimating)
     }
 }
+
+// MARK: - Gesture Support
+extension SpatialObjectsView {
+    
+    var doubleTapGesture: some Gesture {
+        TapGesture(count: 2)
+            .targetedToAnyEntity()
+            .onEnded { value in
+                value.entity.toggleAnimation()
+            }
+    }
+    
+    var tripleTapGesture: some Gesture {
+        TapGesture(count: 3)
+            .targetedToAnyEntity()
+            .onEnded { value in
+                value.entity.removeFromParent()
+            }
+    }
+    
+    var rotateGesture: some Gesture {
+        RotateGesture3D(constrainedToAxis: .y)
+            .targetedToAnyEntity()
+            .onChanged { value in
+                let entity = value.entity
+                if baseTransform == nil {
+                    baseTransform = entity.transform
+                }
+                
+                let transform = Transform(AffineTransform3D(rotation: value.rotation))
+                entity.transform.rotation = baseTransform!.rotation * transform.rotation
+            }
+            .onEnded { _ in
+                baseTransform = nil
+            }
+    }
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .targetedToAnyEntity()
+            .onChanged { value in
+                let entity = value.entity
+                if baseTransform == nil {
+                    baseTransform = entity.transform
+                }
+                
+                let translation = value.convert(value.translation3D, from: .local, to: entity.parent!)
+                entity.transform.translation = baseTransform!.translation + translation
+            }
+            .onEnded { _ in
+                baseTransform = nil
+            }
+    }
+}
+
 
 #Preview("List", windowStyle: .automatic, traits: .fixedLayout(width: 600, height: 800)) {
     let viewModel = CatalogsViewModel()
